ca80a44d090565be5c61a966c54c1cc7857aafff
Jack Parsons <65678430+jackparse14@users.noreply.github.com>
2021-10-22 10:46:43 +0100

    game object, ball & player classes

--- src/MyGame.h
+++ src/MyGame.h
@@ -13,6 +13,45 @@ static struct GameData {
     int ballX = 0;
     int ballY = 0;
 } game_data;
+class GameObject {
+    public:
+        void set_width(int w) {
+            width = w;
+        };
+        void set_height(int h) {
+            height = h;
+        };
+        void set_x_position(int x) {
+            positionX = x;
+        }
+        void set_y_position(int y) {
+            positionY = y;
+        }
+    protected:
+        int positionY;
+        int positionX;
+        int width;
+        int height;
+};
+class Player: public GameObject {
+    public:
+        void set_rect(int x,int y,int w, int h) {
+             set_height(h);
+             set_width(w);
+             set_y_position(y);
+             set_x_position(x);
+             rect = {this->positionX,this->positionY,width,height};
+        }
+        void update_y_position(int y) {
+            set_y_position(y);
+            rect.y = positionY;
+        };
+        SDL_Rect rect;
+};
+class Ball : public GameObject {
+    public:
+        
+};
 
 class MyGame {
     public:
@@ -23,14 +62,15 @@ class MyGame {
         void update();
         void render(SDL_Renderer* renderer);
         void load_media(SDL_Renderer* renderer);
+        void create_game_objects();
         const int windowW = 800;
         const int windowH = 600;
     private:
-        SDL_Rect player1 = { (windowW/4), 0, 20, 60 };
-        SDL_Rect player2 = { ((windowW/4)*3)-20, 0, 20, 60 };
-        SDL_Rect ball = {0,0,10,10};
+        Player player1;
+        Player player2;
+        //SDL_Rect player1 = { (windowW / 4), 0, 20, 60 };
+        //SDL_Rect player2 = { ((windowW / 4) * 3) - 20, 0, 20, 60 };
+        SDL_Rect ball = { 0,0,10,10 };
         SDL_Texture* backgroundTexture;
         SDL_Texture* load_texture(SDL_Renderer* renderer, std::string path);
 };
-
-#endif
